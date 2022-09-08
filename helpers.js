b831512c324e87548cb21301f4fdbf8a5f6e76d0
Sushrut <41235363+sushrutmishra27@users.noreply.github.com>
2022-09-08 17:15:36 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -7,7 +7,7 @@ const getElementByCss = async (driver, name, timeout = defaultTimeout) => {
 	return await driver.wait(until.elementIsVisible(el), timeout);
 };
 
-const getElementByXpath = async (driver, xpath, timeout = defaultTimeout) => {
+const getElementByXpath = async(driver, xpath, timeout = defaultTimeout) => {
 	const el = await driver.wait(until.elementLocated(By.xpath(xpath)), timeout);
 	return await driver.wait(until.elementIsVisible(el), timeout);
 };
